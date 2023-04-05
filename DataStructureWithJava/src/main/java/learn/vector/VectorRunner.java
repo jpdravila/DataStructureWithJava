bd6b7b4d0cdc887d8db8ef8bdcb26fec958d50d7
jpdravila <jpdravila@gmail.com>
2023-04-04 22:22:43 -0400

    Comparing data

--- DataStructureWithJava/src/main/java/learn/vector/VectorRunner.java
+++ DataStructureWithJava/src/main/java/learn/vector/VectorRunner.java
@@ -4,10 +4,12 @@ public class VectorRunner {
     public static void main(String[] args){
         Vector vc = new Vector(5);
 
-        vc.add("Análise 00");
-        vc.add("Análise 01");
-        vc.add("Análise 02");
+        vc.add("Banana");
+        vc.add("Milk");
+        vc.add("Apple");
 
-        System.out.println(vc.busca(1));
+        //System.out.println(vc.busca(1));
+
+        System.out.println(vc.busca("Applejj"));
     }
 }
